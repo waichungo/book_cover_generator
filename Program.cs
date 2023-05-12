0241caf61d03d2c25010d0a425cebe798b31162c
waichungo <waichungojames@gmail.com>
2023-05-12 11:22:51 +0300

    Added PdfPig library

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Program.Start();
+
+public partial class Program
+{
+    public static void Start()
+    {
+        var args = Environment.GetCommandLineArgs();
+        if (args.Length > 1)
+        {
+            var folderToParseBooks = args[0];
+            if (Directory.Exists(folderToParseBooks))
+            {
+                var targetThumbFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "thumbnails");
+                if (!Directory.Exists(targetThumbFolder))
+                {
+                    Directory.CreateDirectory(targetThumbFolder);
+                }
+                var files = Directory.GetFiles(folderToParseBooks, "*", new EnumerationOptions
+                {
+                    IgnoreInaccessible = true
+                }).Where(e => e.ToLower().EndsWith(".pdf") || e.ToLower().EndsWith(".epub")).ToList();
+
+                foreach (var file in files)
+                {
+                    var isPdf = file.ToLower().EndsWith(".pdf");
+                    if (isPdf)
+                    {
+                        
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"'{folderToParseBooks}' does not exist or it is an invalid folder");
+            }
+        }
+    }
+}
